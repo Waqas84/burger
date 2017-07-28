d984ef98071014ee4087c9d4721217c8219a3a57
Waqas84 <walsubayee@yahoo.com>
2017-07-28 13:38:07 -0700

    JAWSDB Added

--- config/connections.js
+++ config/connections.js
@@ -1,33 +1,33 @@
 var mysql = require("mysql");
-// var connection;
+var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//   port: 8889,
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "burgerDB"
-// });
-// };
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 8889,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgerDB"
 });
+};
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// var connection = mysql.createConnection({
+//   port: 8889,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgerDB"
+// });
 
-// connection.connect();
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+connection.connect();
 module.exports = connection;
